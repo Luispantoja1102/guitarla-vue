747b49d56627c4e8fdc5ce876d6efcdb3a703297
luispantoja1102 <l20211821@tectijuana.edu.mx>
2024-05-13 12:25:45 -0700

    Guitar Vue

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<script setup>
+    import {ref, reactive, onMounted} from 'vue';
+    import {db} from './data/guitarras'
+    import Guitarra from './components/guitarra.vue'
+    import Header from './components/header.vue'
+    import Footer from './components/footer.vue'
+
+    const guitarras = ref([])
+    const carrito = ref([])
+    const guitarra = ref({})
+
+
+    onMounted(() => {
+        guitarras.value = db;
+        guitarra.value = db[3];
+    })
+
+    const agregarCarrito = (guitarra) => {
+        const existeCarrito = carrito.value.findIndex(producto => producto.id === guitarra.id)
+        if(existeCarrito >= 0){
+            carrito.value[existeCarrito].cantidad++
+        }
+        else{
+            guitarra.cantidad = 1
+            carrito.value.push(guitarra)
+        }
+    }   
+    const decrementarCantidad= (id)=>{
+        const index = carrito.value.findIndex(producto => producto.id === id)
+        if(carrito.value[index].cantidad <=1) return
+        carrito.value[index].cantidad--
+        console.log(id)
+    }
+    const incrementarCantidad= (id)=>{
+        const index = carrito.value.findIndex(producto => producto.id === id)
+        if(carrito.value[index].cantidad >=5) return
+        carrito.value[index].cantidad++
+        console.log(id)
+    }
+</script>
+
+<template>
+  <Header
+    :carrito="carrito"
+    :guitarra="guitarra"
+    @incrementar-Cantidad="incrementarCantidad"
+    @decrementar-Cantidad="decrementarCantidad"
+    @agregar-Carrito="agregarCarrito"
+  />
+
+  <main class="container-xl mt-5">
+      <h2 class="text-center">Nuestra Colección</h2>
+
+      <div class="row mt-5">
+        <Guitarra 
+            v-for="guitarra in guitarras"
+            v-bind:guitarra = "guitarra"
+            @agregar-Carrito="agregarCarrito"
+        />
+      </div>
+  </main>
+
+
+  <Footer/>
+</template>
+
+<style scoped>
+
+</style>
